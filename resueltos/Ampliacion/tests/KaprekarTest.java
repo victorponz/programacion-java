df808c617f88262cfa32966e13cb4e92ba9a41a8
Víctor Ponz <victorponz@gmail.com>
2023-01-20 10:24:37 +0100

    Modificar fuente

--- resueltos/Ampliacion/tests/KaprekarTest.java
+++ resueltos/Ampliacion/tests/KaprekarTest.java
@@ -10,7 +10,7 @@ public class KaprekarTest {
 
     @Test
     public void addition() {
-        assertEquals(kaprekar.iterations(3524), 3);
+        assertEquals(kaprekar.iterations(3524), 8);
         assertEquals(kaprekar.iterations(1111), 8);
         assertEquals(kaprekar.iterations(1121), 5);
         assertEquals(kaprekar.iterations(3524), 3);
