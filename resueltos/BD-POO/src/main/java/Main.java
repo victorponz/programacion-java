56b7a9928c9ff7199032c57aa9bf8c983a4a767c
Víctor Ponz <victorponz@gmail.com>
2023-04-04 09:23:00 +0200

    Añadir paréntesis

--- resueltos/BD-POO/src/main/java/Main.java
+++ resueltos/BD-POO/src/main/java/Main.java
@@ -3,9 +3,13 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws SQLException{
+        //ESC[38;2;⟨r⟩;⟨g⟩;⟨b⟩
+        final String ANSI_BLACK = "\u001B[38;2;255;0;0m";
+        final String ANSI_BLUE = "\u001B[34m";
+        System.out.println(ANSI_BLACK + "This text is red!");
         HashSet<Usuario> usuarios = UsuarioRepository.getAllUsers();
         for(Usuario usuario: usuarios){
-            System.out.println(usuario);
+           // System.out.println(usuario);
         }
     }
 }
